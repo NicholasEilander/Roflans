af0f8347c50be195166195f4a65576b7a93e7be5
NicholasEilander <4062021@gmail.com>
2025-11-08 00:03:40 +0200

    ну

---
+++ qqqq/qqqq/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int square(int x) {
+	return x * x;
+}
+int main() {
+	int свег;
+	cout << "Лєсссс го:";
+	cin >> свег;
+	cout << "Квадрат свега:" << square(свег) << endl;
+	return 0;
+}
