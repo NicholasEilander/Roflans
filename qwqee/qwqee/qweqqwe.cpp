2fc149e62c614c2a5582929c0e9a29095c228895
Nicholas Eilander(Nik) <90115179+NicholasEilander@users.noreply.github.com>
2025-11-19 00:22:31 +0200

    Variables

--- qwqee/qwqee/qweqqwe.cpp
+++ qwqee/qwqee/qweqqwe.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int гуль, autism;
+	int гуль, zxc;
 	int sum = 0;
-	cout << "Введіть даунізм:";
+	cout << "Введіть uno:";
 	cin >> гуль;
-	cout << "Введіть аутізм:";
-	cin >> autism;
-	for (int i = гуль; i <= autism; i++) {
+	cout << "Введіть dos:";
+	cin >> zxc;
+	for (int i = гуль; i <= zxc; i++) {
 	  if (i % 2 == 0) {
 			sum += i;
 	 }
    }
-	cout << "Сума парних чисел від" << гуль << "до" << autism << sum << endl;
+	cout << "Сума парних чисел від" << гуль << "до" << zxc << sum << endl;
 	return 0;
 
 }
