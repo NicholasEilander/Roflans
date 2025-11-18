b2451cd6300553485d5f4505e4fcd9c8314ada4a
Nicholas Eilander(Nik) <90115179+NicholasEilander@users.noreply.github.com>
2025-11-19 00:19:04 +0200

    Replace variable

--- Povtorka/Povtorka/Source.cpp
+++ Povtorka/Povtorka/Source.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main() {
 	int sum = 0;
-	int autism;
+	int a;
 	cout << "Убийте мене пліз:";
-	cin >> autism;
-	for (int i = 1; i <= autism; i++) {
+	cin >> a;
+	for (int i = 1; i <= a; i++) {
 		sum = sum + i * i;
 	}
-	double avg = (double)sum / autism;
+	double avg = (double)sum / a;
 	cout << "Коли ж прийде фінал:" << avg << endl;
 }
